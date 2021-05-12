0c473c0f6b700831d196d3ee219507cb245290f6
Osti Katriel <ostikatriel@gmail.com>
2021-05-11 20:23:32 -0500

    valores iniciales

--- src/app/subir-archivo/subir-archivo.component.ts
+++ src/app/subir-archivo/subir-archivo.component.ts
@@ -9,24 +9,28 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class SubirArchivoComponent implements OnInit {
 
-  form = new FormGroup({
+  valor:string='two';
+
+  form :FormGroup= this.formBuilder.group({
     input1: new FormControl('', [Validators.required]),
-    select1: new FormControl('', Validators.required),
+    select1: new FormControl(this.valor, Validators.required),
     select2: new FormControl('', [Validators.required]),
-  })
-
-
+    select3: new FormControl('', [Validators.required]),
+    select4: new FormControl('', [Validators.required]),
+  });
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      input1: new FormControl('', [Validators.required]),
-      select1: new FormControl('', Validators.required),
-      select2: new FormControl('', [Validators.required]),
-      select3: new FormControl('', [Validators.required]),
-      select4: new FormControl('', [Validators.required]),
-    });
+
+    if(false){
+      console.log(this.form.value.select1);//solo imprime no setea valor
+      //setear valores
+      this.form.get('select2').setValue("two");
+
+      //this.form.controls.select1.setValue('two')
+    }
+  
   }
 
 
